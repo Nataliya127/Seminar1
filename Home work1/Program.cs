a9a2b2a581f39b327628e94aafd5aaa1fd34380b
Nataliya <Natalia.konyaeva@yandex.ru>
2022-09-03 22:18:16 +0300

    Initial commit

---
+++ Home work1/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine ("Введите число");
+
+string input = Console.ReadLine ();
+
+int number = int.Parse (input);
+
+int squere = number * number;
+
+Console.WriteLine ($"Число в квадрате равно{squere}");
+
+ 
+
+
