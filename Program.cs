364a74704fdf431ce32b5fb720d7218313002253
Nataliya <Natalia.konyaeva@yandex.ru>
2022-09-03 21:09:54 +0300

    Программа для возведения в квадрат

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+Console.WriteLine ("Введите число");
+
+string input = Console.ReadLine ();
+
+int number = int.Parse (input);
+
+int squere = number * number;
+
+Console.WriteLine ($"Число в квадрате равно{squere}");
+
