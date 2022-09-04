85ed69f51d7d3e8f995561e617a969abfbfb9ebb
Nataliya <Natalia.konyaeva@yandex.ru>
2022-09-04 10:12:48 +0300

    нахождение максимального числа

---
+++ Home work2/Program.cs
@@ -0,0 +1,13 @@
+﻿System.Console.WriteLine ("Введите три числа:");
+
+string first = Console.ReadLine ();
+string second = Console.ReadLine();
+string third = Console.ReadLine ();
+
+
+int number1 = int.Parse (first);
+
+int number2 = int.Parse (second);
+
+int number3 = int.Parse (third);
+
